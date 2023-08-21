dea21e429d27be94deb809ae56889bbba94d1b40
Rafael Menas <90731526+MenasRafael@users.noreply.github.com>
2023-08-21 18:26:52 +0000

    adding folder pages

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return (<div>
+        <h1>Tô testando umas parada nova em desenvolvimento, mas olha a foto desse cachorrilho miiii</h1>
+        <img src="/assets/dog.jpg" alt="isso deveria ser um cachorro" />
+    </div>)
+}
+
+export default Home;
